00603c67715f386a5a70457aa3ab33d3d0130104
ot-daurim <to.daurim@gmail.com>
2026-06-13 14:29:39 +0500

    Initialize Spring Boot project foundation

---
+++ src/main/java/com/xfinity/store/XfinityStoreApplication.java
@@ -0,0 +1,13 @@
+package com.xfinity.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XfinityStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XfinityStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xfinity/store/XfinityStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xfinity.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XfinityStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
